dc16c41d55a206431f88c6955cf31ae45c3c0c46
shumssunam <shumssunam15@gmail.com>
2021-01-25 10:41:20 +0600

    station 1

---
+++ app/src/main/java/com/youthfireit/ponnobd/fragments/LoginFragment.java
@@ -0,0 +1,32 @@
+package com.youthfireit.ponnobd.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.youthfireit.ponnobd.R;
+
+public class LoginFragment extends Fragment {
+
+    private View v;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        return v;
+    }
+}
