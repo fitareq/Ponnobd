70ecb731fc7af84bc537f0516b9702079fba5a29
Argento_MadDox <shumssunam15@gmail.com>
2021-02-14 16:45:57 +0600

    Station 2

---
+++ app/src/main/java/com/youthfireit/ponnobd/fragments/SingleProductFragment.java
@@ -0,0 +1,37 @@
+package com.youthfireit.ponnobd.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.youthfireit.ponnobd.R;
+import com.youthfireit.ponnobd.databinding.FragmentSingleProductBinding;
+
+
+public class SingleProductFragment extends Fragment {
+
+
+
+    FragmentSingleProductBinding binding;
+    public SingleProductFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        // Inflate the layout for this fragment
+        binding = FragmentSingleProductBinding.inflate(getLayoutInflater());
+        View v = binding.getRoot();
+
+
+        return v;
+    }
+}
